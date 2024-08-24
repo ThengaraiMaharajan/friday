eb84936b424bd18cfd018a2a8a77547a44ca01c7
DESKTOP-81HU2NM\TMS <tmaharajan1999@gmail.com>
2024-08-25 00:13:30 +0530

    code commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class AppComponent {
 
   isSidebarVisible = false;
+  isUserLoggedIn : boolean = false;
 
   toggleSidebar() {
     this.isSidebarVisible = !this.isSidebarVisible;
