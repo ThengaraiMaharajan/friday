75cfd5c5c6fbc2a04500d775417f291e2dc44130
DESKTOP-81HU2NM\TMS <tmaharajan1999@gmail.com>
2024-09-01 19:38:46 +0530

    registration component created

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     CommonModule,
