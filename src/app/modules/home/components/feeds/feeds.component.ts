761269f3a6d299190c8ff51706f299dfd32195c9
DESKTOP-81HU2NM\TMS <tmaharajan1999@gmail.com>
2024-09-14 14:40:11 +0530

    code commit

---
+++ src/app/modules/home/components/feeds/feeds.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feeds',
+  templateUrl: './feeds.component.html',
+  styleUrl: './feeds.component.css'
+})
+export class FeedsComponent {
+
+}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { DashboardRoutingModule } from './dashboard-routing.module';
+import { HomeRoutingModule } from './home-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    HomeRoutingModule
   ]
 })
-export class DashboardModule { }
+export class HomeModule { }
