d15fbceda42f266b4c7ceb678a20492208b4b69e
DESKTOP-81HU2NM\TMS <tmaharajan1999@gmail.com>
2024-09-15 02:23:36 +0530

    CODE COMMITED

--- src/app/dialogs/login/login.component.ts
+++ src/app/dialogs/login/login.component.ts
@@ -13,7 +13,16 @@ export class LoginComponent {
   loginForm!: FormGroup;
   isLoginFormSubmitted : boolean = false;
 
-  showLoginForm : boolean = true;
+  registrationForm!: FormGroup;
+  isRegistrationFormSubmitted : boolean = false;
+
+  otpForm!: FormGroup;
+  isOtpFormSubmitted : boolean = false;
+
+  passwordForm!: FormGroup;
+  isPasswordFormSubmitted : boolean = false;
+
+  showForm : number = 4;
 
   constructor(
     public dialogRef: MatDialogRef<LoginComponent>,
@@ -22,16 +31,38 @@ export class LoginComponent {
   ) {}
 
   ngOnInit() {
+
+    
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]]
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.minLength(8)]]
     });
+
+    this.registrationForm = this.fb.group({
+      email: [null, [Validators.required, Validators.email]],
+      firstName: [null, [Validators.required, Validators.minLength(2)]],
+      lastName: [null, [Validators.required, Validators.minLength(2)]],
+      phoneNo: [null, [Validators.required, Validators.pattern(/^\d{10}$/)]],
+      countryCode: [null, [Validators.required, Validators.pattern(/^\+\d{1,4}$/)]]
+    });
+
+    this.otpForm = this.fb.group({
+      token: [null, [Validators.required]],
+      code: [null, [Validators.required, Validators.minLength(6), Validators.maxLength(6)]]
+    });
+
+    this.passwordForm = this.fb.group({
+      password: [null, [Validators.required, Validators.minLength(8)]],
+      confirmPassword: [null, [Validators.required, Validators.minLength(8)]]
+    });
+
+
   }
 
-  onSubmit() {
+  loginFormOnSubmit() {
     this.isLoginFormSubmitted = true;
+    console.log('Form submitted:', this.loginForm.value);
     if (this.loginForm.valid) {
-      console.log('Form submitted:', this.loginForm.value);
       let params : any = {
         email : this.loginForm.value.email,
         password : this.loginForm.value.password
@@ -49,12 +80,82 @@ export class LoginComponent {
     }
   }
 
+  registrationFormOnSubmit() {
+    this.isRegistrationFormSubmitted = true;
+    console.log('Form submitted:', this.registrationForm.value);
+    if (this.registrationForm.valid) {
+      let params : any = {
+        email : this.registrationForm.value.email,
+        firstName : this.registrationForm.value.firstName,
+        lastName : this.registrationForm.value.lastName,
+        phoneNo : this.registrationForm.value.phoneNo,
+        countryCode : this.registrationForm.value.countryCode
+      }
+      this.restService.post(params,'/api/user/registration').subscribe(
+        (data : any) => {
+          if(data.rcode === 200){
+            this.otpForm.patchValue({
+              token : data.rObj.token,
+              code : data.rObj.code
+            });
+            this.showForm = 3;
+          }
+        }
+      )
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
+  otpFormOnSubmit() {
+    this.isOtpFormSubmitted = true;
+    console.log('Form submitted:', this.otpForm.value);
+    if (this.otpForm.valid) {
+      let params : any = {
+        token : this.otpForm.value.token,
+        code : this.registrationForm.value.code
+      }
+      this.restService.post(params,'/api/user/verify').subscribe(
+        (data : any) => {
+          if(data.rcode === 200){
+            this.showForm = 4;
+          }
+        }
+      )
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
+  passwordFormOnSubmit() {
+    this.isPasswordFormSubmitted = true;
+    console.log('Form submitted:', this.otpForm.value);
+    if (this.passwordForm.valid) {
+      if(this.passwordForm.value.password === this.passwordForm.value.confirmPassword){
+        let params : any = {
+          password : this.passwordForm.value.password
+        }
+        this.restService.post(params,'/api/user/setpassword').subscribe(
+          (data : any) => {
+            if(data.rcode === 200){
+              
+            }
+          }
+        )
+      }else{
+
+      }
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
   onNoClick(): void {
     this.dialogRef.close();
   }
 
   showRegisterForm(){
-    this.showLoginForm = false;
+    this.showForm = 2;
   }
 
 }
